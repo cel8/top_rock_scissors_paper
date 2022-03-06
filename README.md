cedbe5336fca2b2de2e80ec4aad88f253327f67b
cel8 <alex.celotti@gmail.com>
2022-03-06 10:14:28 -0500

    HTML page with flex style and javascript to play rock scissors paper.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# top_rps
-A rock, scissor or paper fight between man and machine from the Odin Project
+# top_rock_scissors_paper
+A rock, scissors or paper fight between humanity and machines from the <a href="https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/rock-paper-scissors">Odin Project</a> you can <a href="https://cel8.github.io/top_rock_scissors_paper/">check it out</a>.
--- application.js
+++ application.js
@@ -1,53 +1,135 @@
-const move = [ "rock", "paper", "scissor" ];
+// game moves
+const move = [ "rock", "paper", "scissors" ];
+const maxHP = 5;
+// buttons
+const fightButton = document.querySelector(".fight-button");
+const restartButton = document.querySelector(".restart-button");
+// containers
+const gameContainer = document.querySelectorAll(".game");
+const infoContainer = document.querySelectorAll(".info");
+// Score reference
+const refImgRock = document.querySelector("#id-rock");
+const refImgPaper = document.querySelector("#id-paper");
+const refImgScissors = document.querySelector("#id-scissors");
+const refPlayerScore = document.querySelector(".player-score");
+const refComputerScore = document.querySelector(".computer-score");
+const refWinner = document.querySelector(".winner");
+// Global values
+let playerScore;
+let computerScore;
+
+window.addEventListener('load', () => {
+  initialize();
+});
+
+fightButton.addEventListener('click', () => {
+  // hide or show elements 
+  gameContainer.forEach(element => {
+    element.style.display = 'flex';
+  });
+  infoContainer.forEach(element => {
+    element.style.display = 'none';
+  });
+  fightButton.style.display = 'none';
+});
+
+restartButton.addEventListener('click', () => {
+  initialize();
+});
+
+
+// create a new instance of 'MutationObserver' named 'observer', 
+// passing it a callback function
+observer = new MutationObserver(function(mutationsList, observer) {
+  declareWinner();
+});
+
+// call 'observe' on that MutationObserver instance, 
+// passing it the element to observe, and the options object
+observer.observe(refPlayerScore, {characterData: false, childList: true, attributes: false});
+observer.observe(refComputerScore, {characterData: false, childList: true, attributes: false});
+
+function initialize() {
+  infoContainer.forEach(element => {
+    element.style.display = 'block';
+  });
+  fightButton.style.display = 'block';
+  // assign HP
+  playerScore = maxHP;
+  computerScore = maxHP;
+  refPlayerScore.textContent = playerScore;
+  refComputerScore.textContent = computerScore;
+  refWinner.style.display = 'none';
+  restartButton.style.display = 'none';
+}
+
+function declareWinner() {
+  if((playerScore === 0) || (computerScore === 0)) {
+    // Winner text
+    refWinner.textContent = playerScore === 0 ? "You lose..." : "You win!";
+    restartButton.textContent = playerScore === 0 ? "Save the world!" : "Save the world again!";
+    // Remove low hp class
+    if(refComputerScore.classList.contains('low-hp')) {
+      refComputerScore.classList.remove('low-hp');
+      refComputerScore.classList.add('full-hp');
+    }
+    // Remove low hp class
+    if(refPlayerScore.classList.contains('low-hp')) {
+      refPlayerScore.classList.remove('low-hp');
+      refPlayerScore.classList.add('full-hp');
+    }
+    // hide or show elements 
+    gameContainer.forEach(element => {
+      element.style.display = 'none';
+    });
+    refWinner.style.display = 'block';
+    restartButton.style.display = 'block';
+  }
+}
+
+refImgRock.addEventListener('click', () => {
+  playRound(move[0], computerPlay());
+});
+
+refImgPaper.addEventListener('click', () => {
+  playRound(move[1], computerPlay());
+});
+
+refImgScissors.addEventListener('click', () => {
+  playRound(move[2], computerPlay());
+});
 
 function computerPlay() {
+  // get computer move randomly
   let computerMove = Math.floor(Math.random() * move.length);
   return move[computerMove];
 }
 
-function playerPlay() {
-  let playerChoose = prompt("Choose a rock, paper or scissor move!", 'rock');
-  /* Check for no empty string */
-  if(playerChoose) {
-    playerChoose = playerChoose.toLowerCase();
-    if((playerChoose === 'rock') || (playerChoose === 'paper') || (playerChoose === 'scissor')) {
-      return playerChoose;
-    } else {
-      console.log("You should choose between \'rock\', \'paper\' or \'scissor\'!");
-    }
-  }
-  return 'rock';
-}
-
 function playRound(playerSelection, computerSelection) {
-  let winner = -1; // computer win
-  if(playerSelection === computerSelection) {
-    console.log("Even! No one win.");
-    winner = 0;
-  } else {
-    if(((playerSelection === 'rock') && (computerSelection === 'scissor')) ||
-       ((playerSelection === 'scissor') && (computerSelection === 'paper')) ||
-       ((playerSelection === 'paper') && (computerSelection === 'rock'))) {
-      console.log("Player win this round.");
-      winner = 1;
-    } else {
-      console.log("Computer win this round.");
+  // Play until everyone has HP > 0
+  if((playerScore != 0) && (computerScore != 0)) {
+    if(playerSelection !== computerSelection) {
+      if(((playerSelection === 'rock') && (computerSelection === 'scissors')) ||
+        ((playerSelection === 'scissors') && (computerSelection === 'paper')) ||
+        ((playerSelection === 'paper') && (computerSelection === 'rock'))) {
+        computerScore--;
+        refComputerScore.textContent = computerScore;
+        if(computerScore < (maxHP / 2)) {
+          if(refComputerScore.classList.contains('full-hp')) {
+            refComputerScore.classList.remove('full-hp');
+            refComputerScore.classList.add('low-hp');
+          }
+        }
+      } else {
+        playerScore--;
+        refPlayerScore.textContent = playerScore;
+        if(playerScore < (maxHP / 2)) {
+          if(refPlayerScore.classList.contains('full-hp')) {
+            refPlayerScore.classList.remove('full-hp');
+            refPlayerScore.classList.add('low-hp');
+          }
+        }
+      }
     }
-    return winner;
   }
-}
-
-function game() {
-  let playerScore;
-  let computerScore;
-  for(let i = 0; i < 5; ++i) {
-    console.log("Round " + (i + 1));
-    let winner = playRound(playerPlay(), computerPlay());
-    if(winner > 0) playerScore++;
-    else if(winner < 0) computerScore++;
-    else continue;
-  }
-  if(playerScore > computerScore) console.log("Player win!");
-  else if(playerScore < computerScore) console.log("Computer win!");
-  else console.log("No one win!");
 }
